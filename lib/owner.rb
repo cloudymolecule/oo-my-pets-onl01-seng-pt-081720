710c0233de53a0561827e54dc64a7302d4c00b4f
Dario Carlino <baires82@gmail.com>
2020-09-09 19:29:43 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,98 @@
+require "pry"
+
 class Owner
-  # code goes here
+  @@all = []
+  @@count = 0
+  attr_reader :species, :name
+  attr_accessor :cats, :dogs, :mood
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @cats = []
+    @dogs = []
+    @@all << self
+    @@count += 1
+  end
+
+  def cats
+    Cat.all.select do |cat|
+      cat.owner == self
+    end
+  end
+
+  def walk_dogs
+    happy = []
+    Dog.all.select do |dog|
+      if dog.owner == self
+        happy << dog
+      end
+    end
+    happy.each do |hap|
+      hap.mood = "happy"
+    end
+  end
+
+  def feed_cats
+    happy = []
+    Cat.all.select do |cat|
+      if cat.owner == self
+        happy << cat
+      end
+    end
+    happy.each do |hap|
+      hap.mood = "happy"
+    end
+  end
+
+  def dogs
+    Dog.all.select do |dog|
+      dog.owner == self
+    end
+  end
+
+  def buy_cat(name)
+    @cats << Cat.new(name, self)
+  end
+
+  def buy_dog(name)
+    @dogs << Dog.new(name, self)
+  end
+
+  def sell_pets
+    [@cats].each do |sell|
+      sell.mood = "nervous"
+      sell.owner = "nil"
+    end
+    [@dogs].each do |sell|
+      sell.mood = "nervous"
+      sell.owner = "nil"
+    end
+  end
+
+  # def sell_pets
+  #   sad_cats = self.cats
+  #   sad_dogs = self.dogs
+  #   [sad_cats, sad_dogs].each do |sad|
+  #     binding.pry
+  #     sad.mood = "nervous"
+  #   end
+  # end
+
+  def say_species
+     spe = "I am a #{self.species}."
+     spe
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.reset_all
+    @@count = 0
+  end
 end
